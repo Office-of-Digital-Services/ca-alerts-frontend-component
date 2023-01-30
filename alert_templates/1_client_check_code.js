4d55f523fbed651b22a68e7ab2857717f3806c74
Carter Medlin <carterm@users.noreply.github.com>
2023-01-30 09:14:27 -0800

    Wiring up formatting

--- alert_templates/1_client_check_code.js
+++ alert_templates/1_client_check_code.js
@@ -5,35 +5,35 @@ This is the "active" mode code when alerts are in place that checks for dismiss 
 */
 
 (async () => {
-  const messageSourceUrl = `[ALERT_ACTIVE_HTML_URL]`; //This will be set to the HTML URL after the code is minified.
+ const messageSourceUrl = "[ALERT_ACTIVE_HTML_URL]"; //This will be set to the HTML URL after the code is minified.
 
-  const localStorageKey = "CaAlertsLocalStorageMessageDismissed";
+ const localStorageKey = "CaAlertsLocalStorageMessageDismissed";
 
-  const test = 'localStorageTest';
-  try {
-    localStorage.setItem(test, test);
-    localStorage.removeItem(test);
+ const test = "localStorageTest";
+ try {
+  localStorage.setItem(test, test);
+  localStorage.removeItem(test);
 
-    if (localStorage.getItem(localStorageKey) !== messageSourceUrl) {
-      const content = document.createElement("span");
+  if (localStorage.getItem(localStorageKey) !== messageSourceUrl) {
+   const content = document.createElement("span");
 
-      //fetch the html template to render
-      fetch(messageSourceUrl)
-        .then(response => response.text())
-        .then(html => content.innerHTML = html);
+   //fetch the html template to render
+   fetch(messageSourceUrl)
+    .then(response => response.text())
+    .then(html => (content.innerHTML = html));
 
-      content.addEventListener("click", function (e) {
-        if (/** @type {Element} */(e.target).tagName === "BUTTON") {
-          localStorage.setItem(localStorageKey, messageSourceUrl);
-          this.style.display = "none";
-        }
-      });
-
-      //Add the object to the DOM
-      document.body.appendChild(content);
+   content.addEventListener("click", function (e) {
+    if (/** @type {Element} */ (e.target).tagName === "BUTTON") {
+     localStorage.setItem(localStorageKey, messageSourceUrl);
+     this.style.display = "none";
     }
-  } catch (e) {
-    // Local storage does not work here
-    console.error("Alerts can't be displayed.");
+   });
+
+   //Add the object to the DOM
+   document.body.appendChild(content);
   }
+ } catch (e) {
+  // Local storage does not work here
+  console.error("Alerts can't be displayed.");
+ }
 })();
