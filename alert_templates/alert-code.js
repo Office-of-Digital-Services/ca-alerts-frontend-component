450c0f90db800af4b3e8a32e0fa0b08278ab7ec3
Carter Medlin <carterm@users.noreply.github.com>
2023-02-14 16:23:19 -0800

    optimal working version

--- alert_templates/alert-code.js
+++ alert_templates/alert-code.js
@@ -16,18 +16,15 @@ This is the "active" mode code when alerts are in place that checks for dismiss 
 
   if (localStorage.getItem(localStorageKey) !== messageSourceUrl) {
    const containerSpan = document.createElement("span");
-   const closeScript = `<script>function ca_gov_close_alert(x) {document.getElementById('ca_alert_popup_dont_use_id').style.display='none';localStorage.setItem('${localStorageKey}', '${messageSourceUrl}')}</script>`;
 
    //fetch the html template to render
    fetch(messageSourceUrl)
     .then(response => response.text())
     .then(
      html =>
-      (containerSpan.innerHTML = html.replace(
-       // eslint-disable-next-line quotes
-       'srcdoc="',
-       `srcdoc="${closeScript}`
-      ))
+      (containerSpan.innerHTML = html
+       .replace("[LocalStorageKey]", localStorageKey) //The template HTML will be setting local storage to match what we are checking
+       .replace("[LocalStorageValue]", messageSourceUrl)) //Make sure it matches in body content
     );
 
    //Add the object to the DOM
