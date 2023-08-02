a5854d07cc126302bcb194754100d1ec5123c876
Carter Medlin <carterm@users.noreply.github.com>
2023-08-02 10:55:35 -0700

    fix tab width

--- alert_templates/alert-code.js
+++ alert_templates/alert-code.js
@@ -5,29 +5,29 @@ This is the "active" mode code when alerts are in place that checks for dismiss 
 */
 
 (() => {
- const messageSourceUrl = "[ALERT_ACTIVE_MESSAGE_HTML_URL]"; //This will be set to the HTML URL after the code is minified.
- const localStorageKey = "CaAlertsLocalStorageMessageDismissed";
+  const messageSourceUrl = "[ALERT_ACTIVE_MESSAGE_HTML_URL]"; //This will be set to the HTML URL after the code is minified.
+  const localStorageKey = "CaAlertsLocalStorageMessageDismissed";
 
- const localStorageTestValue = "localStorageTest";
- try {
-  // Testing Local Storage compatibility
-  localStorage.setItem(localStorageTestValue, localStorageTestValue);
-  localStorage.removeItem(localStorageTestValue);
+  const localStorageTestValue = "localStorageTest";
+  try {
+    // Testing Local Storage compatibility
+    localStorage.setItem(localStorageTestValue, localStorageTestValue);
+    localStorage.removeItem(localStorageTestValue);
 
-  if (localStorage.getItem(localStorageKey) !== messageSourceUrl) {
-   //fetch the html template to render and put it in the DOM
-   fetch(messageSourceUrl)
-    .then(response => response.text())
-    .then(
-     html =>
-      (document.body.insertBefore(
-       document.createElement("iframe"),
-       document.body.childNodes[0]
-      ).outerHTML = html)
-    );
+    if (localStorage.getItem(localStorageKey) !== messageSourceUrl) {
+      //fetch the html template to render and put it in the DOM
+      fetch(messageSourceUrl)
+        .then(response => response.text())
+        .then(
+          html =>
+            (document.body.insertBefore(
+              document.createElement("iframe"),
+              document.body.childNodes[0]
+            ).outerHTML = html)
+        );
+    }
+  } catch {
+    // Local storage does not work here
+    console.error("Alerts can't be displayed.");
   }
- } catch {
-  // Local storage does not work here
-  console.error("Alerts can't be displayed.");
- }
 })();
