aebffc5d6de009ca680b217a73c4a70a0c239f99
Carter Medlin <carterm@users.noreply.github.com>
2023-08-07 13:02:04 -0700

    using hidden class

--- alert_templates/inner.js
+++ alert_templates/inner.js
@@ -1,29 +1,27 @@
 // @ts-check
 
 ((_window, _document) => {
-  /**
-   * Minify friendly addEventListener
-   */
+  const iFrame = /** @type {HTMLObjectElement} */ (_window.frameElement);
+
+  const toggleHidden = () => {
+    iFrame.classList.toggle("hidden");
+  };
+
   const _addEventListener = (
     /** @type {Element | Window} */ _Element,
     /** @type {string} */ _type,
     /** @type {EventListener} */ _listener
   ) => _Element.addEventListener(_type, _listener);
 
-  const iFrame = /** @type {HTMLObjectElement} */ (_window.frameElement);
-  /**
-   * Set the outer iFrame height to match the inner content
-   */
-  const updateFrameHeight = () => {
-    iFrame.height = `${_document.documentElement.offsetHeight}`;
-    iFrame.className = ""; //Whatever classes we set to hide will be cleared
-  };
+  _addEventListener(_window, "load", toggleHidden);
 
-  // Need to set the frame height when loaded AND during resize
-  _addEventListener(_window, "load", updateFrameHeight);
-  _addEventListener(_window, "resize", updateFrameHeight);
+  _addEventListener(_window, "resize", () => {
+    // Set the outer iFrame height to match the inner content
+    iFrame.height = `${_document.documentElement.offsetHeight}`;
+  });
+
   _addEventListener(_document.querySelector("button"), "click", () => {
-    iFrame.style.display = "none"; //Will not be overridden by a resize to remove the hidden class
+    toggleHidden();
 
     localStorage.setItem(
       "CaAlertsLocalStorageMessageDismissed", //make sure CaAlertsLocalStorageMessageDismissed matches in alert-code.js
