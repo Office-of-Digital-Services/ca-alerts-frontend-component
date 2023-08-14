7ff0dad67725a4e5785fef1582ef8eee0af895f2
Carter Medlin <carterm@users.noreply.github.com>
2023-08-14 12:21:52 -0700

    easier focus event handling

--- alert_templates/inner.js
+++ alert_templates/inner.js
@@ -7,29 +7,24 @@
   const _fixSize = () =>
     (iFrame.height = `${_document.documentElement.offsetHeight}`);
 
-  const _clearClass = () => (iFrame.className = "");
+  const _clearFrameClass = () => (iFrame.className = "");
 
   const _addEventListener = (
-    /** @type {Element | Window} */ _Element,
+    /** @type {Element | Window | Document} */ _Element,
     /** @type {string} */ _type,
     /** @type {EventListener} */ _listener
   ) => _Element.addEventListener(_type, _listener);
 
-  // Hide (Not dismiss) the alert if they tab in and then tab out.  Include everything that could be focused by a screen reader
-  /** @type {Element[]} */
-  const controls = [..._document.querySelectorAll("*")]; //Everything in the alert document
-  controls.forEach(c => {
-    _addEventListener(c, "blur", (/** @type {FocusEvent} */ e) => {
-      if (!controls.includes(/** @type {Element} */ (e.relatedTarget)))
-        iFrame.classList.add("temphidden");
-    });
-
-    _addEventListener(c, "focus", _clearClass);
+  // Hide (Not dismiss) the alert if they tab in and then tab out.
+  _addEventListener(_document, "focusout", (/** @type {FocusEvent} */ e) => {
+    if (!_document.contains(/** @type {Element} */ (e.relatedTarget)))
+      iFrame.classList.add("temphidden");
   });
+  _addEventListener(_document, "focusin", _clearFrameClass);
 
   _addEventListener(_window, "load", () => {
     _fixSize(); //Needed on load for Safari only
-    _clearClass();
+    _clearFrameClass();
   });
 
   _addEventListener(_window, "resize", _fixSize);
