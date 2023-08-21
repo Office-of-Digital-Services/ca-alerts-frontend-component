b405535220ed8376eb7a7808a10bb3b823c23f4e
Carter Medlin <carterm@users.noreply.github.com>
2023-08-21 15:26:54 -0700

    using load event with backup

--- alert_templates/alert-code.js
+++ alert_templates/alert-code.js
@@ -9,32 +9,38 @@ This is the "active" mode code when alerts are in place that checks for dismiss 
  * @param {Storage} _localStorage
  */
 ((_document, _localStorage) => {
-  // If we are viewing the target URL page then don't display anything
-  if (_document.URL == "[ALERT_TARGET_URL]") return;
-  const messageSourceUrl = "[ALERT_ACTIVE_MESSAGE_HTML_URL]"; //This will be set to the HTML URL after the code is minified.
-  const localStorageKey = "CaAlertsLocalStorageMessageDismissed";
-  const documentBody = _document.body; //For minification
-  const localStorageTestValue = `_${localStorageKey}_`;
+  const myEvent = () => {
+    // If we are viewing the target URL page then don't display anything
+    if (_document.URL == "[ALERT_TARGET_URL]") return;
+    const messageSourceUrl = "[ALERT_ACTIVE_MESSAGE_HTML_URL]"; //This will be set to the HTML URL after the code is minified.
+    const localStorageKey = "CaAlertsLocalStorageMessageDismissed";
+    const documentBody = _document.body; //For minification
+    const localStorageTestValue = `_${localStorageKey}_`;
 
-  try {
-    // Testing Local Storage compatibility
-    _localStorage.setItem(localStorageTestValue, localStorageTestValue);
-    _localStorage.removeItem(localStorageTestValue);
+    try {
+      // Testing Local Storage compatibility
+      _localStorage.setItem(localStorageTestValue, localStorageTestValue);
+      _localStorage.removeItem(localStorageTestValue);
 
-    if (_localStorage.getItem(localStorageKey) != messageSourceUrl) {
-      //fetch the html template to render and put it in the DOM
-      fetch(messageSourceUrl)
-        .then(response => response.text())
-        .then(
-          html =>
-            (documentBody.insertBefore(
-              _document.createElement("iframe"),
-              documentBody.firstChild
-            ).outerHTML = html)
-        );
+      if (_localStorage.getItem(localStorageKey) != messageSourceUrl) {
+        //fetch the html template to render and put it in the DOM
+        fetch(messageSourceUrl)
+          .then(response => response.text())
+          .then(
+            html =>
+              (documentBody.insertBefore(
+                _document.createElement("iframe"),
+                documentBody.firstChild
+              ).outerHTML = html)
+          );
+      }
+    } catch {
+      // Local storage does not work here
+      console.error("Alerts can't be displayed.");
     }
-  } catch {
-    // Local storage does not work here
-    console.error("Alerts can't be displayed.");
-  }
+  };
+
+  _document.readyState == "complete"
+    ? myEvent() // Load event is history, just do it
+    : window.addEventListener("load", myEvent);
 })(document, localStorage);
