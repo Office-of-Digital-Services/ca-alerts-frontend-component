2a7e144b380f96d891acfcbf47ee2aca076db66f
Carter Medlin <carterm@users.noreply.github.com>
2023-08-21 14:07:42 -0700

    load event mapped

--- alert_templates/alert-code.js
+++ alert_templates/alert-code.js
@@ -5,41 +5,40 @@ This is the "active" mode code when alerts are in place that checks for dismiss 
 */
 
 window.addEventListener("load", () => {
-  /**
-   * @param {Document} _document
-   * @param {Storage} _localStorage
-   */
-  ((_document, _localStorage) => {
-    const messageSourceUrl = "[ALERT_ACTIVE_MESSAGE_HTML_URL]"; //This will be set to the HTML URL after the code is minified.
-    const targetURL = "[ALERT_TARGET_URL]";
-    const localStorageKey = "CaAlertsLocalStorageMessageDismissed";
-    const documentBody = _document.body;
+  /** @type {Document} */
+  const _document = document;
+  /** @type {Storage} */
+  const _localStorage = localStorage;
 
-    const localStorageTestValue = `_${localStorageKey}_`;
+  const messageSourceUrl = "[ALERT_ACTIVE_MESSAGE_HTML_URL]"; //This will be set to the HTML URL after the code is minified.
+  const targetURL = "[ALERT_TARGET_URL]";
+  const localStorageKey = "CaAlertsLocalStorageMessageDismissed";
+  const documentBody = _document.body;
 
-    //If we are viewing the target URL page then don't display anything
-    if (_document.URL == targetURL) return;
+  const localStorageTestValue = `_${localStorageKey}_`;
 
-    try {
-      // Testing Local Storage compatibility
-      _localStorage.setItem(localStorageTestValue, localStorageTestValue);
-      _localStorage.removeItem(localStorageTestValue);
+  //If we are viewing the target URL page then don't display anything
+  if (_document.URL == targetURL) return;
 
-      if (_localStorage.getItem(localStorageKey) != messageSourceUrl) {
-        //fetch the html template to render and put it in the DOM
-        fetch(messageSourceUrl)
-          .then(response => response.text())
-          .then(
-            html =>
-              (documentBody.insertBefore(
-                _document.createElement("iframe"),
-                documentBody.firstChild
-              ).outerHTML = html)
-          );
-      }
-    } catch {
-      // Local storage does not work here
-      console.error("Alerts can't be displayed.");
+  try {
+    // Testing Local Storage compatibility
+    _localStorage.setItem(localStorageTestValue, localStorageTestValue);
+    _localStorage.removeItem(localStorageTestValue);
+
+    if (_localStorage.getItem(localStorageKey) != messageSourceUrl) {
+      //fetch the html template to render and put it in the DOM
+      fetch(messageSourceUrl)
+        .then(response => response.text())
+        .then(
+          html =>
+            (documentBody.insertBefore(
+              _document.createElement("iframe"),
+              documentBody.firstChild
+            ).outerHTML = html)
+        );
     }
-  })(document, localStorage);
+  } catch {
+    // Local storage does not work here
+    console.error("Alerts can't be displayed.");
+  }
 });
