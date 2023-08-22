95649f8aea7cbeed116714b3627f912ad2316ee7
Carter Medlin <carterm@users.noreply.github.com>
2023-08-22 08:49:03 -0700

    minified support

--- alert_templates/alert-code.js
+++ alert_templates/alert-code.js
@@ -4,25 +4,17 @@
 This is the "active" mode code when alerts are in place that checks for dismiss before rendering
 */
 
-/**
- * @param {Document} _document
- * @param {Storage} _localStorage
- */
-((_document, _localStorage) => {
-  const myEvent = () => {
-    // If we are viewing the target URL page then don't display anything
-    if (_document.URL == "[ALERT_TARGET_URL]") return;
-    const messageSourceUrl = "[ALERT_ACTIVE_MESSAGE_HTML_URL]"; //This will be set to the HTML URL after the code is minified.
-    const localStorageKey = "CaAlertsLocalStorageMessageDismissed";
-    const documentBody = _document.body; //For minification
-    const localStorageTestValue = `_${localStorageKey}_`;
-
+((_document, _localStorage, messageSourceUrl) => {
+  const localStorageKey = "CaAlertsLocalStorageMessageDismissed";
+  const documentBody = _document.body; //For minification
+  const localStorageTestValue = `_${localStorageKey}_`;
+  const loadAlert = () => {
     try {
       // Testing Local Storage compatibility
       _localStorage.setItem(localStorageTestValue, localStorageTestValue);
       _localStorage.removeItem(localStorageTestValue);
 
-      if (_localStorage.getItem(localStorageKey) != messageSourceUrl) {
+      if (_localStorage.getItem(localStorageKey) != messageSourceUrl)
         //fetch the html template to render and put it in the DOM
         fetch(messageSourceUrl)
           .then(response => response.text())
@@ -33,14 +25,15 @@ This is the "active" mode code when alerts are in place that checks for dismiss 
                 documentBody.firstChild
               ).outerHTML = html)
           );
-      }
     } catch {
       // Local storage does not work here
       console.error("Alerts can't be displayed.");
     }
   };
+  // If we are viewing the target URL page then don't display anything
+  if (_document.URL != "[ALERT_TARGET_URL]") return;
 
   _document.readyState == "complete"
-    ? myEvent() // Load event is history, just do it
-    : window.addEventListener("load", myEvent);
-})(document, localStorage);
+    ? loadAlert() // Load event is history, inline execution, just do it
+    : window.addEventListener("load", loadAlert);
+})(document, localStorage, "[ALERT_ACTIVE_MESSAGE_HTML_URL]");
